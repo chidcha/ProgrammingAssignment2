7578d2538ecda26da374b891321b8e62945b6374
chidcha <cchoksuchat@hotmail.com>
2018-02-17 14:55:04 +0700

    2 commits

--- cachematrix.R
+++ cachematrix.R
@@ -1,26 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Computing the inverse of a square matrix can be done with the solve function in R
+## Matrix inversion is usually a costly computation and there may be some benefit to caching the inverse of a matrix rather than compute it repeatedly.
 
-## Write a short comment describing this function
+## makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
   inverse <- NULL
-  set <- function(y){
-    x <<- y
+  
+  ## set the value of the matrix
+  
+  set <- function(w){
+    x <<- w
     inverse <<- NULL
   }
+  
+  ## get the value of the matrix
+  
   get <- function() x
+  
+  ## set the value of the inverse
+  
   setInverse <- function(resultMatrix) inverse <<- resultMatrix
+  
+  ## get the value of the inverse
+  
   getInverse <- function() inverse
   list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+##If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
   inverse <- x$getInverse()
+  
+  ##The following function calculates the inverse of the special "matrix" created with the above function. 
+  
+  ##However, it first checks to see if the inverse has already been calculated. 
+  ##If so, it gets the inverse from the cache and skips the computation. 
+  ##Otherwise, it calculates the inverse of the data and sets the value of the inverse in the cache via the setInverse function.
+  
   if(!is.null(inverse)){
     message("...getting cached data")
     return(inverse)
